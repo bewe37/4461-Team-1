0824e3d0e51305f2fd0dc4990864caae20307930
qannevu <qannevu@my.yorku.ca>
2025-03-16 22:00:22 -0400

    updated agents to move every other step
    allows belief of humans changing to be visible, removed repeated code, changed logic of humans belief changing

--- src/aiChatterHumanScatter/agents.py
+++ src/aiChatterHumanScatter/agents.py
@@ -4,14 +4,14 @@ import random
 class SocialMediaUser(Agent):
     """
     A Schelling-style agent with a 'belief' attribute.
-    type = 1 => bot (disinformed), type = 0 => human (initially factual).
+    type = 1 => disinformed/disinformational, type = 0 => informed/informational.
     """
 
     def __init__(self, model, agent_type: int):
         super().__init__(model)
         self.type = agent_type
-        # 30% of bots started as disinformed, 70% informed / factual
-        # Humans start factual (belief=0).
+        # 30% chance for a bot to be informational
+        # Humans start informed (belief=0).
         if agent_type == 1:
             self.belief = 0 if random.random() < 0.3 else 1
             self.activated = False 
@@ -20,6 +20,7 @@ class SocialMediaUser(Agent):
             self.activated = True
 
         self.activated = False if agent_type == 1 else True
+        self.step_counter = -1 # Counter to track steps
 
     def step(self):
    
@@ -38,26 +39,29 @@ class SocialMediaUser(Agent):
                 fraction_bots = bot_neighbors / len(neighbors)
 
                 # If fewer than 50% of neighbors are bots, move
-                if fraction_bots < 0.5:
-                    self.model.move_to_empty(self)
-                
-                # 30% chance of the bots spreading factual information
-                for neighbor in neighbors:
-                    if neighbor.type == 0:
-                        neighbor.belief = 0 if random.random() < 0.3 else 1
+                if self.step_counter % 2 == 0:
+                    if fraction_bots < 0.5:
+                        self.model.move_to_empty(self)
+
         else:
             # HUMAN behavior
             bot_neighbors = sum(1 for n in neighbors if n.type == 1)
-            # If there's at least one bot neighbor, humans exposed to misinformation.
+            # If there's at least one bot neighbor, 30% chance humans belief changes.
             if bot_neighbors > 0 and random.random() < 0.3:
-                self.belief = 1
+                if self.belief == 1:
+                    self.belief = 0
+                else:
+                    self.belief = 1
 
             same_belief_neighbors = sum(1 for n in neighbors if n.belief == self.belief)
             similarity_fraction = same_belief_neighbors / len(neighbors)
 
             if similarity_fraction < self.model.homophily:
-                # 50% chance to leave the ecosystem
-                self.model.move_to_empty(self)
+                # Chance to move if unhappy
+                if self.step_counter % 2 == 0:
+                    self.model.move_to_empty(self)
             else:
                 # Agent is happy, increment the model's happy counter
                 self.model.happy += 1
+
+        self.step_counter += 1
